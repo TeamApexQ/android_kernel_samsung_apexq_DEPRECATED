4c603dc91a691e0619f59205b7f1265a2a9a381e
noptys <mail@noptys.com>
2012-12-01 14:34:09 -0600

    apexq: mrproper pre-patch

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Wed Sep 5 16:18:57 KST 2012"
-#define LINUX_COMPILE_BY "jryukim"
-#define LINUX_COMPILE_HOST "SystemSW-jryukim-BuildPC"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
