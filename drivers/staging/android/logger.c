f9088053abc06293f9d9756d5685b2834c471100
infernix <infernix@gmail.com>
2013-04-26 03:29:19 +0200

    fix SEC_DEBUG

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -771,7 +771,7 @@ static int __init init_log(struct logger_log *log)
 int sec_debug_subsys_set_logger_info(
 	struct sec_debug_subsys_logger_log_info *log_info)
 {
-	struct secdbg_logger_log_info log_info = {
+	struct sec_debug_subsys_logger_log_info log_info = {
 		.stinfo = {
 			.buffer_offset = offsetof(struct logger_log, buffer),
 			.w_off_offset = offsetof(struct logger_log, w_off),
