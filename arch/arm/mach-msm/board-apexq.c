428e5358114101501a93f6893db898a99e3d38b4
infernix <infernix@gmail.com>
2013-05-09 18:19:15 +0200

    Increase size of ION AUDIO carveout to 5MB * Needed for proper playback on LPA

--- arch/arm/mach-msm/board-apexq.c
+++ arch/arm/mach-msm/board-apexq.c
@@ -297,7 +297,7 @@ static struct msm_gpiomux_config msm8960_sec_ts_configs[] = {
 
 
 #define MSM_PMEM_ADSP_SIZE         0x4E00000 /* 78 Mbytes */
-#define MSM_PMEM_AUDIO_SIZE        0x160000 /* 1.375 Mbytes */
+#define MSM_PMEM_AUDIO_SIZE        0x4CF000 /* 5 Mbytes */
 #define MSM_PMEM_SIZE 0x2800000 /* 40 Mbytes */
 #define MSM_LIQUID_PMEM_SIZE 0x4000000 /* 64 Mbytes */
 #define MSM_HDMI_PRIM_PMEM_SIZE 0x4000000 /* 64 Mbytes */
