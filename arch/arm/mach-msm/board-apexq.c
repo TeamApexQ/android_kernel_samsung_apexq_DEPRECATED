f5acf4fdb0e8f0672f3a1ba4e4b6df2a06b83379
David Daynard <nardholio@gmail.com>
2013-07-28 20:01:16 -0400

    swap device init back like stock + upstream
    Not sure why d2 has these switched

--- arch/arm/mach-msm/board-apexq.c
+++ arch/arm/mach-msm/board-apexq.c
@@ -4579,8 +4579,8 @@ static struct platform_device *common_devices[] __initdata = {
 
 static struct platform_device *apexq_devices[] __initdata = {
 	&msm_8960_q6_lpass,
-	&msm_8960_q6_mss_sw,
 	&msm_8960_q6_mss_fw,
+	&msm_8960_q6_mss_sw,
 	&msm_8960_riva,
 	&msm_pil_tzapps,
 	&msm_pil_vidc,
